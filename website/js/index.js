2e638ac5eea77cb8189af7bd53a4c9ffd33d845c
Eugene Cheung <ey.cheung@outlook.com>
2015-10-31 12:38:37 -0400

    Minor website edits

--- website/js/index.js
+++ website/js/index.js
@@ -1,6 +1,6 @@
-var DatePicker = require('../../src/anytime')
+var Anytime = require('../../src/anytime')
   , time = document.getElementsByClassName('js-splash-input')[0]
   , button = document.getElementsByClassName('js-splash-button')[0]
-  , d = new DatePicker({ input: time, button: button, anchor: button })
+  , picker = new Anytime({ input: time, button: button, anchor: button })
 
-d.render()
+picker.render()
